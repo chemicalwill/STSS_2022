bfa07a102aee78133f090f4a0f63aee5e87f593d
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2022-04-28 15:43:20 -0500

    Formatted with black

--- STSS_2022.py
+++ STSS_2022.py
@@ -29,10 +29,7 @@ def check_for_normalcy(df, alpha, col):
 
 
 def get_fisher_exact_results_as_dict(a, b, c, d):
-    table = np.array([
-        [a, b],
-        [c, d]
-    ])
+    table = np.array([[a, b], [c, d]])
 
     if any([a == 0, b == 0, c == 0, d == 0]):
         a += 0.5
@@ -43,13 +40,13 @@ def get_fisher_exact_results_as_dict(a, b, c, d):
     odds_ratio, pvalue = stats.fisher_exact(table)
     odds_ratio = round(odds_ratio, 2)
     pvalue = round_to_3_decimal_or_sci_places(pvalue)
-    
-    lowerci = np.exp(np.log(odds_ratio) - 1.96 * sqrt(1/a + 1/b + 1/c + 1/d))
-    upperci = np.exp(np.log(odds_ratio) + 1.96 * sqrt(1/a + 1/b + 1/c + 1/d))
+
+    lowerci = np.exp(np.log(odds_ratio) - 1.96 * sqrt(1 / a + 1 / b + 1 / c + 1 / d))
+    upperci = np.exp(np.log(odds_ratio) + 1.96 * sqrt(1 / a + 1 / b + 1 / c + 1 / d))
     lowerci, upperci = round(lowerci, 2), round(upperci, 2)
 
     return {"OR": odds_ratio, "p-value": pvalue, "95% CI": f"{lowerci} - {upperci}"}
-        
+
 
 def extract_2_groups_from_df(df, group_col, value):
     a, b = sorted(df[group_col].unique())
@@ -65,11 +62,8 @@ def get_ag_results_as_dict(df, group_col, value):
 
 
 def get_barnard_exact_results_as_dict(a, b, c, d):
-    table = np.array([
-        [a, b],
-        [c, d]
-    ])
-    
+    table = np.array([[a, b], [c, d]])
+
     ber = stats.barnard_exact(table)
     stat = round(ber.statistic, 3)
     p = round_to_3_decimal_or_sci_places(ber.pvalue)
@@ -81,7 +75,7 @@ def get_kruskall_wallis_results_as_dict(df, group_col, value):
     group1, group2 = extract_2_groups_from_df(df, group_col, value)
     kw_h_statistic, pvalue = stats.kruskal(group1, group2)
     kw_h_statistic = round(kw_h_statistic, 3)
-    pvalue = round_to_3_decimal_or_sci_places(pvalue)   
+    pvalue = round_to_3_decimal_or_sci_places(pvalue)
     return {"Kruskall-Wallis H statistic": kw_h_statistic, "p-value": pvalue}
 
 
@@ -90,7 +84,7 @@ def get_one_way_anova_results_as_dict(df, group_col, value):
     anova_stat, pvalue = stats.f_oneway(group1, group2)
     anova_stat = round(anova_stat, 3)
     pvalue = round_to_3_decimal_or_sci_places(pvalue)
-    return {"One-Way ANOVA": anova_stat, "p-value": pvalue} 
+    return {"One-Way ANOVA": anova_stat, "p-value": pvalue}
 
 
 def get_t_test_results_as_dict(df, group_col, value):
@@ -98,8 +92,9 @@ def get_t_test_results_as_dict(df, group_col, value):
     group1, group2 = extract_2_groups_from_df(df, group_col, value)
     pass
 
+
 def write_out_results(fp, residents):
-    with open(fp, 'w') as f:
+    with open(fp, "w") as f:
         for resident in residents:
             for name, results in resident.items():
                 f.write(name.title())
@@ -108,7 +103,6 @@ def write_out_results(fp, residents):
                     for stat, value in result.items():
                         f.write(f" {stat} {value},")
                 f.write("\n\n")
-
     return
 
 
@@ -116,8 +110,8 @@ def seth():
     """
     PRIMARY OUTCOME - RATE OF GP CONTAM.
             +------+------+
-            | BCC- | BCC+ | 
-            +------+------+   
+            | BCC- | BCC+ |
+            +------+------+
     COVID19+|  69  |  64  |
             +------+------+
     COVID19-| 823  | 266  |
@@ -125,7 +119,7 @@ def seth():
 
     SECONDARY OUTCOME - RATE OF ECHO
             +-------+-------+
-            | ECHO- | ECHO+ | 
+            | ECHO- | ECHO+ |
             +-------+-------+
     COVID19+|   63  |   1   |
             +-------+-------+
@@ -133,22 +127,24 @@ def seth():
             +-------+-------+
 
     """
-    rate_of_gp_contam = get_fisher_exact_results_as_dict(69, 64, 823, 266) # FIXME
+    rate_of_gp_contam = get_fisher_exact_results_as_dict(69, 64, 823, 266)
     rate_of_echo = get_fisher_exact_results_as_dict(63, 1, 249, 17)
-    df = pd.read_csv('seth_data_cleaned.csv')
+    df = pd.read_csv("seth_data_cleaned.csv")
     check_for_normalcy(df, 0.05, "length_ofstay")
-    length_of_stay = get_kruskall_wallis_results_as_dict(df, "covid_positive", "length_ofstay")
+    length_of_stay = get_kruskall_wallis_results_as_dict(
+        df, "covid_positive", "length_ofstay"
+    )
     return {
         "Seth": {
             "Rate of GP contamination": rate_of_gp_contam,
             "Rate of ECHO": rate_of_echo,
-            "Length of stay": length_of_stay
+            "Length of stay": length_of_stay,
         }
     }
 
 
 def sarah():
-    """ 
+    """
     PRIMARY OUTCOME - CHF EXAC
             +---------+---------+
             | NO EXAC |  EXACS  |
@@ -184,23 +180,23 @@ def sarah():
         "Sarah": {
             "Rate of CHF exacerbation": rate_of_chf_exac,
             "Rate of CV death": rate_of_cv_death,
-            "Death from any cause": death_from_any_cause
+            "Death from any cause": death_from_any_cause,
         }
     }
 
 
 def sean():
-    df = pd.read_csv('sean_data_cleaned.csv')
+    df = pd.read_csv("sean_data_cleaned.csv")
     check_for_normalcy(df, 0.05, "combo_time_delta")
     check_for_normalcy(df, 0.05, "total_oic_visit_time")
-    premed_time = get_kruskall_wallis_results_as_dict(df, "combo_batched_yn", "combo_time_delta")
-    oic_chair_time = get_kruskall_wallis_results_as_dict(df, "combo_batched_yn", "total_oic_visit_time")
-    return {
-        "Sean": {
-            "Premed time": premed_time,
-            "OIC chair time": oic_chair_time
-        }
-    }
+    premed_time = get_kruskall_wallis_results_as_dict(
+        df, "combo_batched_yn", "combo_time_delta"
+    )
+    oic_chair_time = get_kruskall_wallis_results_as_dict(
+        df, "combo_batched_yn", "total_oic_visit_time"
+    )
+    return {"Sean": {"Premed time": premed_time, "OIC chair time": oic_chair_time}}
+
 
 def main():
     # suppress RuntimeWarning for zero division + move into project dir
@@ -208,7 +204,7 @@ def main():
     warnings.filterwarnings("ignore", category=RuntimeWarning)
     os.chdir(Path(__file__).parent)
 
-    write_out_results('results.txt', [seth(), sarah(), sean()])
+    write_out_results("results.txt", [seth(), sarah(), sean()])
     return
 
 
