cf46e51317114c4aec09cb7f115950fe1b7415a2
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2022-04-28 11:58:29 -0500

    Overhaul + results
    * added functions for multiple tests
    * grouped resident analyses in functions
    * added function to write out results
    * updated comments

--- STSS_2022.py
+++ STSS_2022.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from math import sqrt
-import pprint
+import os
+from pathlib import Path
 import warnings
 
 import numpy as np
@@ -14,6 +15,17 @@ def round_to_3_decimal_or_sci_places(pvalue):
         return "{:0.2e}".format(pvalue)
     else:
         return round(pvalue, 3)
+
+
+def check_for_normalcy(df, alpha, col):
+    k2, p = stats.normaltest(df[col])
+    if p < alpha:
+        p = round_to_3_decimal_or_sci_places(p)
+        print(f"{col} is normally distributed, p = {p}")
+    else:
+        p = round_to_3_decimal_or_sci_places(p)
+        print(f"Warning: {col} is NOT normally distributed, p = {p}")
+    return
 
 
 def get_fisher_exact_results_as_dict(a, b, c, d):
@@ -30,103 +42,159 @@ def get_fisher_exact_results_as_dict(a, b, c, d):
         upperci = np.exp(np.log(odds_ratio) + 1.96 * sqrt(1/a + 1/b + 1/c + 1/d))
         lowerci, upperci = round(lowerci, 2), round(upperci, 2)
     except ZeroDivisionError:
-        # lowerci, upperci = None, None
+        # TODO make these 2 lines build a dict rather than make 2 different ones
         return {"OR": odds_ratio, "p-value": pvalue}
     else:
         return {"OR": odds_ratio, "p-value": pvalue, "95% CI": f"{lowerci} - {upperci}"}
         
 
-def get_kruskall_wallis_results_as_dict(df, col1, col2):
-    statistic, pvalue = stats.kruskal(df[col1], df[col2])
-    statistic = round(statistic, 3)
+def extract_2_groups_from_df(df, group_col, value):
+    a, b = sorted(df[group_col].unique())
+    a = df.loc[df[group_col] == a, value]
+    b = df.loc[df[group_col] == b, value]
+    return (a, b)
+
+
+def get_ag_results_as_dict(df, group_col, value):
+    group1, group2 = extract_2_groups_from_df(df, group_col, value)
+    # FIXME would like to figure out how to split up results
+    return {"Alexander Govern results": stats.alexandergovern(group1, group2)}
+
+
+def get_kruskall_wallis_results_as_dict(df, group_col, value):
+    group1, group2 = extract_2_groups_from_df(df, group_col, value)
+    kw_h_statistic, pvalue = stats.kruskal(group1, group2)
+    kw_h_statistic = round(kw_h_statistic, 3)
+    pvalue = round_to_3_decimal_or_sci_places(pvalue)   
+    return {"Kruskall-Wallis H statistic": kw_h_statistic, "p-value": pvalue}
+
+
+def get_one_way_anova_results_as_dict(df, group_col, value):
+    group1, group2 = extract_2_groups_from_df(df, group_col, value)
+    anova_stat, pvalue = stats.f_oneway(group1, group2)
+    anova_stat = round(anova_stat, 3)
     pvalue = round_to_3_decimal_or_sci_places(pvalue)
-    return {"statistic": statistic, "p-value": pvalue}
+    return {"One-Way ANOVA": anova_stat, "p-value": pvalue} 
 
 
-# suppress RuntimeWarning for zero division
-#   ex. Sarah's A groups for Fisher's Exact Test div/0 when calculating 95% CI
-warnings.filterwarnings("ignore", category=RuntimeWarning)
+def get_t_test_results_as_dict(df, group_col, value):
+    # TODO
+    group1, group2 = extract_2_groups_from_df(df, group_col, value)
+    pass
 
-### SETH ###
-"""
-PRIMARY OUTCOME - RATE OF GP CONTAM.
-        +----------+----------+
-        | COVID19+ | COVID19- | 
-        +----------+----------+   
-    +BCC|    64    |   266    |
-        +----------+----------+
-    -BCC|    69    |   823    |
-        +----------+----------+
+def write_out_results(fp, residents):
+    with open(fp, 'w') as f:
+        for resident in residents:
+            for name, results in resident.items():
+                f.write(name.title())
+                for endpoint, result in results.items():
+                    f.write(f"\n - {endpoint}: ")
+                    for stat, value in result.items():
+                        f.write(f" {stat}: {value}, ")
+                f.write("\n\n")
+        
+        # TODO remove the trailing commas...
+    return
 
-SECONDARY OUTCOME - RATE OF ECHO
-        +----------+----------+
-        | COVID19+ | COVID19- | 
-        +----------+----------+
-    ECHO|    1     |   17     |
-        +----------+----------+
-    NONE|    63    |   249    |
-        +----------+----------+
 
-"""
-rate_of_gp_contam = get_fisher_exact_results_as_dict(64, 266, 69, 823)
-rate_of_echo = get_fisher_exact_results_as_dict(1, 17, 63, 249)
-"""
-# TODO
-df = pd.read_csv('/path/to/seths/data')
-length_of_stay = get_kruskall_wallis_results_as_dict(df, col1, col2)
-"""
-seths_results = {
-    "Rate of GP contamination": rate_of_gp_contam,
-    "Rate of ECHO": rate_of_echo,
-    "Length of stay": {} # length_of_stay
-}
+def seth():
+    """
+    PRIMARY OUTCOME - RATE OF GP CONTAM.
+            +----------+----------+
+            | COVID19+ | COVID19- | 
+            +----------+----------+   
+        +BCC|    64    |   266    |
+            +----------+----------+
+        -BCC|    69    |   823    |
+            +----------+----------+
 
-### SARAH ###
-""" 
-PRIMARY OUTCOME - CHF EXAC
-        +-----------+-----------+
-        | SGLT2+S/V | S/V ALONE |
-        +-----------+-----------+
-    EXAC|    12          27     |
-        +-----------+-----------+
-    NONE|    50          100    |
-        +-----------+-----------+
+    SECONDARY OUTCOME - RATE OF ECHO
+            +----------+----------+
+            | COVID19+ | COVID19- | 
+            +----------+----------+
+        ECHO|    1     |   17     |
+            +----------+----------+
+        NONE|    63    |   249    |
+            +----------+----------+
 
-SECONDARY OUTCOME - CV DEATH
-        +-----------+-----------+
-        | SGLT2+S/V | S/V ALONE |
-        +-----------+-----------+
-    DEAD|     0     |     2     |
-        +-----------+-----------+
-    LIVE|    62     |    127    |
-        +-----------+-----------+
+    """
+    rate_of_gp_contam = get_fisher_exact_results_as_dict(64, 266, 69, 823)
+    rate_of_echo = get_fisher_exact_results_as_dict(1, 17, 63, 249)
+    df = pd.read_csv('seth_data_cleaned.csv')
+    check_for_normalcy(df, 0.05, "length_ofstay")
+    length_of_stay = get_kruskall_wallis_results_as_dict(df, "covid_positive", "length_ofstay")
+    return {
+        "Seth": {
+            "Rate of GP contamination": rate_of_gp_contam,
+            "Rate of ECHO": rate_of_echo,
+            "Length of stay": length_of_stay
+        }
+    }
 
-SECONDARY - OVERALL DEATH
-        +-----------+-----------+
-        | SGLT2+S/V | S/V ALONE |
-        +-----------+-----------+
-    DEAD|     0     |     10    |
-        +-----------+-----------+
-    LIVE|    62     |    127    |
-        +-----------+-----------+
-"""
-rate_of_chf_exac = get_fisher_exact_results_as_dict(12, 27, 50, 100)
-rate_of_cv_death = get_fisher_exact_results_as_dict(0, 2, 62, 127)
-death_from_any_cause = get_fisher_exact_results_as_dict(0, 10, 62, 127)
-sarahs_results = {
-    "Rate of CHF exacerbation": rate_of_chf_exac,
-    "Rate of CV death": rate_of_cv_death,
-    "Death from any cause": death_from_any_cause
-}
-print(pprint.pformat(sarahs_results))
 
-### SEAN ###
-"""
-df = pd.read_csv('/path/to/seans/data') # FIXME
-premed_time = get_kruskall_wallis_results_as_dict(df, col1, col2)
-oic_chair_time = get_kruskall_wallis_results_as_dict(df, col1, col2)
-seans_results = {
-    "Primary endpoint": premed_time,
-    "OIC chair time": oic_chair_time
-}
-"""
+def sarah():
+    """ 
+    PRIMARY OUTCOME - CHF EXAC
+            +-----------+-----------+
+            | SGLT2+S/V | S/V ALONE |
+            +-----------+-----------+
+        EXAC|    12          27     |
+            +-----------+-----------+
+        NONE|    50          100    |
+            +-----------+-----------+
+
+    SECONDARY OUTCOME - CV DEATH
+            +-----------+-----------+
+            | SGLT2+S/V | S/V ALONE |
+            +-----------+-----------+
+        DEAD|     0     |     2     |
+            +-----------+-----------+
+        LIVE|    62     |    127    |
+            +-----------+-----------+
+
+    SECONDARY - OVERALL DEATH
+            +-----------+-----------+
+            | SGLT2+S/V | S/V ALONE |
+            +-----------+-----------+
+        DEAD|     0     |     10    |
+            +-----------+-----------+
+        LIVE|    62     |    127    |
+            +-----------+-----------+
+    """
+    rate_of_chf_exac = get_fisher_exact_results_as_dict(12, 27, 50, 100)
+    rate_of_cv_death = get_fisher_exact_results_as_dict(0, 2, 62, 127)
+    death_from_any_cause = get_fisher_exact_results_as_dict(0, 10, 62, 127)
+    return {
+        "Sarah": {
+            "Rate of CHF exacerbation": rate_of_chf_exac,
+            "Rate of CV death": rate_of_cv_death,
+            "Death from any cause": death_from_any_cause
+        }
+    }
+
+
+def sean():
+    df = pd.read_csv('sean_data_cleaned.csv')
+    check_for_normalcy(df, 0.05, "combo_time_delta")
+    check_for_normalcy(df, 0.05, "total_oic_visit_time")
+    premed_time = get_kruskall_wallis_results_as_dict(df, "combo_batched_yn", "combo_time_delta")
+    oic_chair_time = get_kruskall_wallis_results_as_dict(df, "combo_batched_yn", "total_oic_visit_time")
+    return {
+        "Sean": {
+            "Premed time": premed_time,
+            "OIC chair time": oic_chair_time
+        }
+    }
+
+def main():
+    # suppress RuntimeWarning for zero division + move into project dir
+    #   ex. Sarah's A groups for Fisher's Exact Test div/0 when calculating 95% CI
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
+    os.chdir(Path(__file__).parent)
+
+    write_out_results('results.txt', [seth(), sarah(), sean()])
+    return
+    
+
+if __name__ == "__main__":
+    main()
